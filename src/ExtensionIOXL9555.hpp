49f74f09a3c5c36861fdfc8f98edd7beac36bfb5
lewis <lewishe@outlook.com>
2023-04-02 17:00:47 +0800

    Rename tpp to hpp

--- src/ExtensionIOXL9555.hpp
+++ src/ExtensionIOXL9555.hpp
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * @file      ExtensionIOXL9555.tpp
+ * @file      ExtensionIOXL9555.hpp
  * @author    Lewis He (lewishe@outlook.com)
  * @date      2022-12-27
  *
--- src/SensorPCF8563.hpp
+++ src/SensorPCF8563.hpp
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * @file      SensorPCF8563.tpp
+ * @file      SensorPCF8563.hpp
  * @author    Lewis He (lewishe@outlook.com)
  * @date      2022-12-09
  *
