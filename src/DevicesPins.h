414832bf8af10c80eff04f026acd3706098f41cc
lewisxhe <lewishe@outlook.com>
2024-09-21 17:24:30 +0800

    [DevicesPin]Update pins to digital

--- src/DevicesPins.h
+++ src/DevicesPins.h
@@ -1,8 +1,29 @@
 /**
- * @file      DevicesPins.h
+ *
+ * @license MIT License
+ *
+ * Copyright (c) 2024 lewis he
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file      TouchDrv_GT9895_GetPoint.ino
  * @author    Lewis He (lewishe@outlook.com)
- * @license   MIT
- * @copyright Copyright (c) 2024  ShenZhen XinYuan Electronic Technology Co., Ltd
  * @date      2024-09-14
  *
  */
@@ -25,8 +46,8 @@
 // T-Display-AMOLED 1.91 Inch QWIIC
 #define SENSOR_SDA 3
 #define SENSOR_SCL 2
-#define SENSOR_IRQ TX
-#define SENSOR_RST RX
+#define SENSOR_IRQ 43//TX
+#define SENSOR_RST 44//RX
 #elif defined(ARDUINO_T_AMOLED_241)
 // T4-S3 CST226SE ✅
 #define SENSOR_SDA 6
