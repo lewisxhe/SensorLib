372f3043197b586005718044e773f42b1bd2c9e3
lewisxhe <lewishe@outlook.com>
2024-12-30 09:26:19 +0800

    Update version to v0.2.5

--- src/SensorLib_Version.h
+++ src/SensorLib_Version.h
@@ -35,7 +35,7 @@
 /** Minor version number (x.X.x) */
 #define SENSORLIB_VERSION_MINOR   2
 /** Patch version number (x.x.X) */
-#define SENSORLIB_VERSION_PATCH   4
+#define SENSORLIB_VERSION_PATCH   5
 
 /**
  * Macro to convert SENSORLIB version number into an integer
@@ -53,4 +53,4 @@
                                              SENSORLIB_VERSION_MINOR, \
                                              SENSORLIB_VERSION_PATCH)
 
-#define SENSORLIB_VERSION_STR   "v0.2.4"
+#define SENSORLIB_VERSION_STR   "v0.2.5"
