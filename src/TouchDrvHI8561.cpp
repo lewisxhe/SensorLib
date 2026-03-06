a2795ad625c5e979e7419760e32a9206c8b62cd1
lewisxhe <lewishe@outlook.com>
2026-03-06 14:23:31 +0800

    Added new touch panel HI8561 support [no ci] https://github.com/lewisxhe/SensorLib/issues/33

---
+++ src/TouchDrvHI8561.cpp
@@ -0,0 +1,99 @@
+#include "TouchDrvHI8561.hpp"
+
+void TouchDrvHI8561::reset()
+{
+    if (_rst != -1) {
+        hal->digitalWrite(_rst, LOW);
+        hal->delay(3);
+        hal->digitalWrite(_rst, HIGH);
+        hal->delay(5);
+    }
+}
+
+void TouchDrvHI8561::sleep()
+{
+}
+
+void TouchDrvHI8561::wakeup()
+{
+    reset();
+}
+
+const TouchPoints &TouchDrvHI8561::getTouchPoints()
+{
+    static TouchPoints points;
+
+    points.clear(); // Clear cached touch points
+
+    uint8_t buffer[53] = {0};
+
+    if (makePacketThenRead(TOUCH_INFO_START_ADDR, buffer, arraySize(buffer))) {
+
+        uint8_t numPoints = buffer[0];
+        if (numPoints == 0 || numPoints > MAX_FINGER_NUM) {
+            return points;
+        }
+
+        for (uint8_t i = 0; i < numPoints; i++) {
+            const uint8_t idx = BYTES_OFFSET + i * BYTES_PER_POINT;
+            uint16_t x = buffer[idx + 1] | (buffer[idx + 0] << 8);
+            uint16_t y = buffer[idx + 3] | (buffer[idx + 2] << 8);
+            uint8_t  pressure = buffer[idx + 4];
+            // Edge trigger detection
+            if (x == UINT16_MAX || y == UINT16_MAX) {
+                log_e("Edge trigger detected, id %d set to invalid coordinates", i);
+                break;
+            }
+            points.addPoint(x, y, pressure, i);
+        }
+        // Swap XY or mirroring coordinates,if set
+        updateXY(points);
+    }
+    return points;
+}
+
+bool TouchDrvHI8561::isPressed()
+{
+    if (_irq != -1) {
+        return hal->digitalRead(_irq) == LOW;
+    }
+    return getTouchPoints().hasPoints();
+}
+
+const char *TouchDrvHI8561::getModelName()
+{
+    return "HI8561";
+}
+
+bool TouchDrvHI8561::initImpl(uint8_t addr)
+{
+
+    if (_irq != -1) {
+        hal->pinMode(_irq, INPUT);
+    }
+
+    if (_rst != -1) {
+        hal->pinMode(_rst, OUTPUT);
+    }
+
+    reset();
+
+    // Disable I2C stop bit
+    I2CParam params(I2CParam::I2C_SET_FLAG, false);
+    comm->setParams(params);
+
+    // Check if the starting address matches
+    uint8_t buffer[6];
+    if (!makePacketThenRead(SECTION_INFO_START_ADDR + 8, buffer, sizeof(buffer))) {
+        return false;
+    }
+    uint32_t reg = buffer[0] + (buffer[1] << 8) + (buffer[2] << 16) + (buffer[3] << 24);
+    if (reg != TOUCH_INFO_START_ADDR) {
+        log_e("Invalid touch info start address: 0x%08X", reg);
+        return false;
+    }
+
+    log_d("HI8561 touch start address: 0x%08X", reg);
+
+    return true;
+}
---
+++ src/TouchDrvHI8561.hpp
@@ -0,0 +1,131 @@
+/**
+ *
+ * @license MIT License
+ *
+ * Copyright (c) 2026 lewis he
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file      TouchDrvHI8561.hpp
+ * @author    Lewis He (lewishe@outlook.com)
+ * @date      2026-03-06
+ *
+ */
+#pragma once
+
+#include "TouchDrvInterface.hpp"
+
+#define HI8561_SLAVE_ADDRESS     (0x68)
+
+/**
+* @brief HI8561 Touchscreen Display Chip Touch Class
+* This class provides the touch reading functionality for the HI8561.
+* Note: This class cannot be used independently; the screen must be
+* initialized first before calling this class for initialization.
+* This is a characteristic of most touchscreen display chips.
+*/
+class TouchDrvHI8561 :  public TouchDrvInterface
+{
+public:
+    /**
+    * @brief  Constructor for the touch driver
+    * @retval None
+    */
+    TouchDrvHI8561() = default;
+
+    /**
+    * @brief  Destructor for the touch driver
+    * @retval None
+    */
+    ~TouchDrvHI8561() = default;
+
+    /**
+    * @brief  Reset the touch driver
+    * @note   This function will reset the touch driver by toggling the reset pin.
+    * @retval None
+    */
+    void reset() override;
+
+    /**
+    * @brief Puts the touch driver to sleep
+    * @note The chip integrates touch and display functions, and a sleep mode cannot be set.
+    * @retval None
+    */
+    void sleep() override;
+
+    /**
+     * @brief  Wake up the touch driver
+     * @note   The chip integrates touch and display functions, and a sleep mode cannot be set.
+     * @retval None
+     */
+    void wakeup() override;
+
+    /**
+    * @brief  Get the touch points
+    * @note   This function will retrieve the touch points from the touch driver.
+    *         TouchPoints is configured with a 5-point touch point buffer by default,
+    *         so it can return touch data from a maximum of 5 points. Although the HI8561
+    *         supports 10-point touch
+    * @retval A reference to the touch points.
+    */
+    const TouchPoints &getTouchPoints() override;
+
+    /**
+    * @brief  Check if the touch point is pressed
+    * @note   This function will check if the touch point is currently pressed.
+    * @retval True if the touch point is pressed, false otherwise.
+    */
+    bool isPressed() override;
+
+    /**
+    * @brief  Get the model name
+    * @note   This function will retrieve the model name from the touch driver.
+    * @retval The model name.
+    */
+    const char *getModelName() override;
+
+private:
+
+    /**
+     * @brief  Initialize the touch driver
+     * @note   This function will initialize the touch driver by setting up the I2C communication.
+     * @param  addr: The I2C address of the touch driver.
+     * @retval True if the initialization was successful, false otherwise.
+     */
+    bool initImpl(uint8_t addr) override;
+
+    bool inline makePacketThenRead(uint32_t addr, uint8_t *data, uint16_t len)
+    {
+        uint8_t addrBuf[6];
+        addrBuf[0] = 0xF3;
+        addrBuf[1] = (uint8_t)((addr & 0xFF000000) >> 24);
+        addrBuf[2] = (uint8_t)((addr & 0x00FF0000) >> 16);
+        addrBuf[3] = (uint8_t)((addr & 0x0000FF00) >> 8);
+        addrBuf[4] = (uint8_t)((addr & 0x000000FF) >> 0);
+        addrBuf[5] = 0x03;
+        return comm->writeThenRead(addrBuf, sizeof(addrBuf), data, len) == 0;
+    }
+
+protected:
+    static constexpr uint32_t SECTION_INFO_START_ADDR = 0x200110D0;
+    static constexpr uint32_t TOUCH_INFO_START_ADDR = 0x20011120;
+    static constexpr uint8_t BYTES_OFFSET = 3;
+    static constexpr uint8_t MAX_FINGER_NUM = (10);
+    static constexpr uint8_t BYTES_PER_POINT = (5);
+};
