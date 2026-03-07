f336b7c7147c8e0b5c03afae5f7ab9cc7fe3ad49
lewisxhe <lewishe@outlook.com>
2026-03-07 15:07:30 +0800

    Remove error check[no ci]

--- src/platform/espidf/SensorCommEspIDF_HW.hpp
+++ src/platform/espidf/SensorCommEspIDF_HW.hpp
@@ -63,7 +63,7 @@ public:
             config.pull_up_en = GPIO_PULLUP_DISABLE;
             config.pull_down_en = GPIO_PULLDOWN_DISABLE;
             config.intr_type = GPIO_INTR_DISABLE;
-            ESP_ERROR_CHECK(gpio_config(&config));
+            gpio_config(&config);
         }
     }
 
